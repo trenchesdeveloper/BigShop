5ff4af3c118f6974f002df3d9a5337fbd408fed0
Damilare <opeyemisamuel222@gmail.com>
2021-02-07 14:56:55 +0000

    setting up router

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,20 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter as Router,Switch, Route } from "react-router-dom";
+import Login from "./pages/auth/Login";
+import Register from "./pages/auth/Register";
+import Home from "./pages/Home";
 
 const App = () => {
-  return (
-    <div>
-      App
-    </div>
-  )
-}
 
-export default App
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/register" component={Register} />
+        <Route exact path="/" component={Home} />
+      </Switch>
+    </Router>
+  );
+};
+
+export default App;
