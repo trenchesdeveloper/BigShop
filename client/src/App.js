e0d3851d70a9cc93c8ff5eae7bf3b34bb09914d1
Damilare <opeyemisamuel222@gmail.com>
2021-02-08 11:14:39 +0000

    header component done

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,22 @@
 import React from "react";
-import { BrowserRouter as Router,Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Header from "./components/Nav/Header";
 import Login from "./pages/auth/Login";
 import Register from "./pages/auth/Register";
 import Home from "./pages/Home";
 
 const App = () => {
-
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/login" component={Login} />
-        <Route exact path="/register" component={Register} />
-        <Route exact path="/" component={Home} />
-      </Switch>
-    </Router>
+    <>
+      <Header />
+      <Router>
+        <Switch>
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/register" component={Register} />
+          <Route exact path="/" component={Home} />
+        </Switch>
+      </Router>
+    </>
   );
 };
 
--- client/src/components/Nav/Header.js
+++ client/src/components/Nav/Header.js
@@ -22,10 +22,8 @@ const Header = () => {
       </Menu.Item>
 
       <SubMenu key="SubMenu" icon={<SettingOutlined />} title="Register">
-        <Menu.ItemGroup title="Item 1">
-          <Menu.Item key="setting:1">Option 1</Menu.Item>
-          <Menu.Item key="setting:2">Option 2</Menu.Item>
-        </Menu.ItemGroup>
+        <Menu.Item key="setting:1">Option 1</Menu.Item>
+        <Menu.Item key="setting:2">Option 2</Menu.Item>
       </SubMenu>
     </Menu>
   );
