dbe886dfc1b9ee3a55afe5afa2b4551abf075677
Damilare <opeyemisamuel222@gmail.com>
2021-02-08 11:01:29 +0000

    Header setup

---
+++ client/src/components/Nav/Header.js
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import { Menu } from "antd";
+import {
+  MailOutlined,
+  AppstoreOutlined,
+  SettingOutlined,
+} from "@ant-design/icons";
+const { SubMenu } = Menu; // or Menu.subMenu
+
+const Header = () => {
+  const [current, setCurrect] = useState("");
+
+  // create click handler
+  const handleClick = () => {
+    //
+  };
+
+  return (
+    <Menu onClick={handleClick} selectedKeys={[current]} mode="horizontal">
+      <Menu.Item key="mail" icon={<MailOutlined />}>
+        Home
+      </Menu.Item>
+
+      <SubMenu key="SubMenu" icon={<SettingOutlined />} title="Register">
+        <Menu.ItemGroup title="Item 1">
+          <Menu.Item key="setting:1">Option 1</Menu.Item>
+          <Menu.Item key="setting:2">Option 2</Menu.Item>
+        </Menu.ItemGroup>
+      </SubMenu>
+    </Menu>
+  );
+};
+
+export default Header;
