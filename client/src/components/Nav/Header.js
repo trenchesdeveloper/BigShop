d0efe8540ac56e46c83d075cc921f6317858b7e0
Damilare <opeyemisamuel222@gmail.com>
2021-02-09 14:03:05 +0000

    conditional render the username

--- client/src/components/Nav/Header.js
+++ client/src/components/Nav/Header.js
@@ -56,7 +56,7 @@ const Header = () => {
       )}
 
       {user && (
-        <SubMenu key="SubMenu" icon={<SettingOutlined />} title={} className='float-right'>
+        <SubMenu key="SubMenu" icon={<SettingOutlined />} title={user && user.email.split('@')[0]} className='float-right'>
           <Item key="setting:1">Option 1</Item>
           <Item key="setting:2">Option 2</Item>
           <Item icon={<LogoutOutlined />} onClick={logoutHandler}>
