3d4a11d819de098eb1272a32a04620866b916fa7
Damilare <opeyemisamuel222@gmail.com>
2021-02-09 12:36:26 +0000

    login functionality

--- client/src/pages/auth/Login.js
+++ client/src/pages/auth/Login.js
@@ -1,17 +1,40 @@
 import React, { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
 import { toast } from "react-toastify";
 import { auth } from "../../firebase";
 import { Button } from "antd";
 import { MailOutlined } from "@ant-design/icons";
+import { loginUser } from "../../actions/userActions";
 
-const RegisterComplete = ({ history }) => {
+const Login = ({ history }) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const dispatch = useDispatch();
 
   // handler
   const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log(email, password);
+    setLoading(true);
+    try {
+      const { user } = await auth.signInWithEmailAndPassword(email, password);
+      const idTokenResult = await user.getIdTokenResult();
+
+      // dispatch the action
+      dispatch(
+        loginUser({
+          email: user.email,
+          token: idTokenResult.token,
+        })
+      );
+
+      history.push("/");
+    } catch (error) {
+      console.log(error);
+      toast.error(error.message);
+      setLoading(false);
+    }
   };
 
   return (
@@ -46,6 +69,7 @@ const RegisterComplete = ({ history }) => {
               shape="round"
               size="large"
               disabled={!email || password.length < 6}
+              onClick={handleSubmit}
             >
               Login with Email/Password
             </Button>
@@ -56,4 +80,4 @@ const RegisterComplete = ({ history }) => {
   );
 };
 
-export default RegisterComplete;
+export default Login;
