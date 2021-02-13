7f07ae8f80bbff6db28459c17ac7f524b36948f9
Damilare <opeyemisamuel222@gmail.com>
2021-02-13 11:45:25 +0000

    role checking endpoint

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -1,5 +1,7 @@
 import asyncHandler from "express-async-handler";
 import admin from "../utils/firebase.js";
+import User from "../models/userModel.js";
+import AppError from '../utils/appError.js'
 
 export const authCheck = asyncHandler(async (req, res, next) => {
   // 1) Get the authToken from req.headers
@@ -10,5 +12,23 @@ export const authCheck = asyncHandler(async (req, res, next) => {
   // 3) add it to the req object
   req.user = firebaseUser;
 
+  next();
+});
+
+export const adminCheck = asyncHandler(async (req, res, next) => {
+  // 1) Get the authToken from req.headers
+  const { email } = req.user;
+
+  const adminUser = await User.findOne({ email });
+
+  if (!adminUser) {
+    return next(new AppError("user not found", 404));
+  }
+
+  // check if the role is admin
+  if(adminUser !== 'admin'){
+    return next(new AppError("not authorized", 403));
+  }
+  
   next();
 });
--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,11 +1,13 @@
 import express from "express";
 import { createOrUpdateUser, currentUser } from "../controllers/authController.js";
-import { authCheck } from "../middlewares/auth.js";
+import { adminCheck, authCheck } from "../middlewares/auth.js";
 
 const router = express.Router();
 
 router.post("/createOrUpdateUser", authCheck, createOrUpdateUser);
 
 router.get('/currentUser', authCheck, currentUser);
+
+router.get("/currentAdmin", authCheck,adminCheck, currentUser);
 
 export default router;
