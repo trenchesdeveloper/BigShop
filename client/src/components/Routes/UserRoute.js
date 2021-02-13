3ee818e4ca62cc5875eded42f86bb15a7d60996a
Damilare <opeyemisamuel222@gmail.com>
2021-02-13 15:17:01 +0000

    clean up

--- client/src/components/Routes/UserRoute.js
+++ client/src/components/Routes/UserRoute.js
@@ -1,14 +1,13 @@
 import React from "react";
-import { Link, Route } from "react-router-dom";
+import { Route } from "react-router-dom";
 import { useSelector } from "react-redux";
-import Loader from "../Loader";
 import LoadingToRedirect from "./LoadingToRedirect";
 
 const UserRoute = ({ children, ...rest }) => {
   const { userInfo } = useSelector((state) => state.userLogin);
 
   return userInfo && userInfo.token ? (
-    <Route {...rest}  />
+    <Route {...rest} />
   ) : (
     <LoadingToRedirect />
   );
