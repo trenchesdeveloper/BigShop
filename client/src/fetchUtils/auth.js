2771f5b56d8738d79d271b86cc495bc31a0bd558
Damilare <opeyemisamuel222@gmail.com>
2021-02-14 09:04:06 +0000

    crud operations for category in client-side

--- client/src/fetchUtils/auth.js
+++ client/src/fetchUtils/auth.js
@@ -1,16 +1,51 @@
-import axios from 'axios'
+import axios from "axios";
 
 export const createOrUpdateUser = async (authtoken) => {
-  return 
+  return;
 };
 
+export const currentAdmin = async (token) => {
+  const config = {
+    headers: {
+      token,
+    },
+  };
 
-export const currentAdmin = async (token) =>{
-const config = {
-  headers: {
-    token
-  }
-}
+  return await axios.get(`/api/auth/currentAdmin`, config);
+};
 
-  return await axios.get(`/api/auth/currentAdmin`, config)
-}
+export const createCategory = async (token, name) => {
+  const config = {
+    headers: {
+      token,
+    },
+  };
+
+  return await axios.post(`/api/category`, { name }, config);
+};
+
+export const getCategories = async () => {
+  return await axios.get(`/api/category`);
+};
+
+export const getCategory = async (slug) => {
+  return await axios.get(`/api/category/${slug}`);
+};
+
+export const updateCategory = async (slug, token, name) => {
+  const config = {
+    headers: {
+      token,
+    },
+  };
+  return await axios.put(`/api/category/${slug}`, { name }, config);
+};
+
+export const deleteCategory = async (slug, token) => {
+  const config = {
+    headers: {
+      token,
+    },
+  };
+  return await axios.delete(`/api/category/${slug}`, config);
+};
