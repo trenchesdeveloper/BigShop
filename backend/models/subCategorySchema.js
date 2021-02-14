68d00e14283deee4eee0fc7f9ed1c7b8f16203a0
Damilare <opeyemisamuel222@gmail.com>
2021-02-14 17:40:53 +0000

    sub-category model

---
+++ backend/models/subCategorySchema.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+import slugify from "slugify";
+
+const subCategorySchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      required: [true, "name is required"],
+      minlength: [3, "Too short"],
+      maxlength: [32, "Too long"],
+    },
+    slug: {
+      type: String,
+      lowercase: true,
+      //unique: true,
+      //index: true,
+    },
+    parent: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Category",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+categorySchema.pre("save", async function (next) {
+  this.slug = await slugify(this.name);
+  next();
+});
+
+const SubCategory = mongoose.model("SubCategory", subCategorySchema);
+
+export default SubCategory;
