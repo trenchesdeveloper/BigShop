672c4edee3c30fb8d9cb26d314583549d0a146aa
Damilare <opeyemisamuel222@gmail.com>
2021-02-14 10:38:26 +0000

    create category form

--- client/src/pages/admin/category/CategoryCreate.js
+++ client/src/pages/admin/category/CategoryCreate.js
@@ -1,15 +1,45 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import AdminNav from "../../../components/Nav/AdminNav";
-
+import { toast } from "react-toastify";
+import { useSelector } from "react-redux";
+import {
+  createCategory,
+  getCategories,
+  deleteCategory,
+} from "../../../fetchUtils/auth";
 
 const CategoryCreate = () => {
+  const [name, setName] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(name);
+  };
+
   return (
     <div className="container-fluid">
       <div className="row">
         <div className="col-md-2">
           <AdminNav />
         </div>
-        <div className="col">Category create page</div>
+        <div className="col">
+          <h4>Create Category</h4>
+          <form onSubmit={handleSubmit}>
+            <div className="form-group">
+              <label htmlFor="">Name</label>
+              <input
+                type="text"
+                value={name}
+                className="form-control"
+                onChange={(e) => setName(e.target.value)}
+                autoFocus
+                required
+              />
+              <br />
+              <button className="btn btn-outline-primary  ">Save</button>
+            </div>
+          </form>
+        </div>
       </div>
     </div>
   );
