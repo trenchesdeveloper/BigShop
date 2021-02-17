bb309182d101175559d4e8b6aa88801f22257219
Damilare <opeyemisamuel222@gmail.com>
2021-02-17 14:02:07 +0000

    product create form completed

--- client/src/pages/admin/product/ProductCreate.js
+++ client/src/pages/admin/product/ProductCreate.js
@@ -49,9 +49,7 @@ const ProductCreate = () => {
     //
   };
 
-  const handleChange = (e) => {
-
-  };
+  const handleChange = (e) => {};
 
   return (
     <div className="container-fluid">
@@ -85,6 +83,74 @@ const ProductCreate = () => {
                 onChange={handleChange}
               />
             </div>
+
+            <div className="form-group">
+              <label htmlFor="title">Price</label>
+              <input
+                type="number"
+                name="price"
+                value={price}
+                className="form-control"
+                onChange={handleChange}
+              />
+            </div>
+
+            <div className="form-group">
+              <label htmlFor="title">Shipping</label>
+              <select
+                name="shipping"
+                id=""
+                className="form-control"
+                onChange={handleChange}
+              >  
+                <option>Please select</option>
+                <option value="No">No</option>
+                <option value="Yes">Yes</option>
+              </select>
+            </div>
+
+            <div className="form-group">
+              <label htmlFor="quantity">Quantity</label>
+              <input
+                type="number"
+                name="quantity"
+                value={quantity}
+                className="form-control"
+                onChange={handleChange}
+              />
+            </div>
+
+            <div className="form-group">
+              <label htmlFor="color">Color</label>
+              <select
+                name="color"
+                id=""
+                className="form-control"
+                onChange={handleChange}
+              >
+                <option>Please select</option>
+              {colors.map(color=> (
+                <option value={color} key={color}>{color}</option>
+              ))}
+              </select>
+            </div>
+
+             <div className="form-group">
+              <label htmlFor="brand">Brand</label>
+              <select
+                name="brand"
+                id=""
+                className="form-control"
+                onChange={handleChange}
+              >
+                <option>Please select</option>
+              {brands.map(b=> (
+                <option value={b} key={b}>{b}</option>
+              ))}
+              </select>
+            </div>
+
+            <button className="btn btn-outline-info">Create</button>
           </form>
         </div>
       </div>
