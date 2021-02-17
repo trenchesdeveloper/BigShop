0b0236e3a8a9650c83fc8c0cfef5988db935f0bf
Damilare <opeyemisamuel222@gmail.com>
2021-02-17 13:27:22 +0000

    description input field

--- client/src/pages/admin/product/ProductCreate.js
+++ client/src/pages/admin/product/ProductCreate.js
@@ -13,6 +13,7 @@ const initialState = {
   title: "",
   description: "",
   price: "",
+  categories: [],
   category: "",
   sub: [],
   shipping: "",
@@ -26,6 +27,31 @@ const initialState = {
 
 const ProductCreate = () => {
   const [values, setValues] = useState(initialState);
+  const {
+    title,
+    description,
+    price,
+    categories,
+    category,
+    sub,
+    shipping,
+    quantity,
+    images,
+    colors,
+    color,
+    brands,
+    brand,
+  } = values;
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    //
+  };
+
+  const handleChange = (e) => {
+
+  };
 
   return (
     <div className="container-fluid">
@@ -33,7 +59,34 @@ const ProductCreate = () => {
         <div className="col-md-2">
           <AdminNav />
         </div>
-        <div className="col-md-10">Product create form</div>
+        <div className="col-md-10">
+          <h4>Product Create</h4>
+          <hr />
+
+          <form onSubmit={handleSubmit}>
+            <div className="form-group">
+              <label htmlFor="title">Title</label>
+              <input
+                type="text"
+                name="title"
+                value={title}
+                className="form-control"
+                onChange={handleChange}
+              />
+            </div>
+
+            <div className="form-group">
+              <label htmlFor="description">Description</label>
+              <input
+                type="text"
+                name="description"
+                value={description}
+                className="form-control"
+                onChange={handleChange}
+              />
+            </div>
+          </form>
+        </div>
       </div>
     </div>
   );
