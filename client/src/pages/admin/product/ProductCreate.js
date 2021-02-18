7f330e1e54971ae4b0d99e64037f035397963091
Damilare <opeyemisamuel222@gmail.com>
2021-02-18 09:32:10 +0000

    refactor the form for create product

--- client/src/pages/admin/product/ProductCreate.js
+++ client/src/pages/admin/product/ProductCreate.js
@@ -9,6 +9,7 @@ import { EditOutlined, DeleteOutlined } from "@ant-design/icons";
 import CategoryForm from "../../../components/forms/CategoryForm";
 import LocalSearch from "../../../components/forms/LocalSearch";
 import { productCreate } from "../../../actions/productActions";
+import ProductCreateForm from "../../../components/forms/ProductCreateForm";
 
 const initialState = {
   title: "",
@@ -28,21 +29,7 @@ const initialState = {
 
 const ProductCreate = () => {
   const [values, setValues] = useState(initialState);
-  const {
-    title,
-    description,
-    price,
-    categories,
-    category,
-    sub,
-    shipping,
-    quantity,
-    images,
-    colors,
-    color,
-    brands,
-    brand,
-  } = values;
+  
 
   const dispatch = useDispatch();
 
@@ -85,101 +72,11 @@ const ProductCreate = () => {
           <h4>Product Create</h4>
           <hr />
 
-          <form onSubmit={handleSubmit}>
-            <div className="form-group">
-              <label htmlFor="title">Title</label>
-              <input
-                type="text"
-                name="title"
-                value={title}
-                className="form-control"
-                onChange={handleChange}
-              />
-            </div>
-
-            <div className="form-group">
-              <label htmlFor="description">Description</label>
-              <input
-                type="text"
-                name="description"
-                value={description}
-                className="form-control"
-                onChange={handleChange}
-              />
-            </div>
-
-            <div className="form-group">
-              <label htmlFor="title">Price</label>
-              <input
-                type="number"
-                name="price"
-                value={price}
-                className="form-control"
-                onChange={handleChange}
-              />
-            </div>
-
-            <div className="form-group">
-              <label htmlFor="title">Shipping</label>
-              <select
-                name="shipping"
-                id=""
-                className="form-control"
-                onChange={handleChange}
-              >
-                <option>Please select</option>
-                <option value="No">No</option>
-                <option value="Yes">Yes</option>
-              </select>
-            </div>
-
-            <div className="form-group">
-              <label htmlFor="quantity">Quantity</label>
-              <input
-                type="number"
-                name="quantity"
-                value={quantity}
-                className="form-control"
-                onChange={handleChange}
-              />
-            </div>
-
-            <div className="form-group">
-              <label htmlFor="color">Color</label>
-              <select
-                name="color"
-                id=""
-                className="form-control"
-                onChange={handleChange}
-              >
-                <option>Please select</option>
-                {colors.map((c) => (
-                  <option value={c} key={c}>
-                    {c}
-                  </option>
-                ))}
-              </select>
-            </div>
-
-            <div className="form-group">
-              <label htmlFor="brand">Brand</label>
-              <select
-                name="brand"
-                id=""
-                className="form-control"
-                onChange={handleChange}
-              >
-                <option>Please select</option>
-                {brands.map((b) => (
-                  <option value={b} key={b}>
-                    {b}
-                  </option>
-                ))}
-              </select>
-            </div>
-
-            <button className="btn btn-outline-info">Create</button>
-          </form>
+          <ProductCreateForm
+            handleSubmit={handleSubmit}
+            handleChange={handleChange}
+            values ={values}
+          />
         </div>
       </div>
     </div>
