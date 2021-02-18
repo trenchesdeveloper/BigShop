68043eb411bfc5bcbaeaef6d6aee46a463abef72
Damilare <opeyemisamuel222@gmail.com>
2021-02-18 18:23:31 +0000

    cloudinary controller completed

--- backend/controllers/cloudinary.js
+++ backend/controllers/cloudinary.js
@@ -1,5 +1,5 @@
 import asyncHandler from "express-async-handler";
-import cloudinary from 'cloudinary'
+import cloudinary from "cloudinary";
 import AppError from "../utils/appError";
 
 // config
@@ -9,20 +9,30 @@ cloudinary.config({
   api_secret: process.env.CLOUDINARY_SECRET,
 });
 
-export const upload = asyncHandler (async (req, res, next) =>{
-const result = await cloudinary.uploader.upload(req.body.image, {
+export const upload = asyncHandler(async (req, res, next) => {
+  const result = await cloudinary.v2.uploader.upload(req.body.image, {
     public_id: `${Date.now()}`,
-    resource_type: 'auto',
+    resource_type: "auto",
+  });
 
-})
+  if (!result) {
+    return next(new AppError(`Error with cloudinary`, 400));
+  }
 
-if(!result){
-    next(new AppError(`Error with cloudinary`, 400))
-}
+  res.status(200).json({
+    public_id: result.public_id,
+    url: result.secure_url,
+  });
+});
 
+export const remove = asyncHandler(async (req, res, next) => {
+    const imageId = req.body.public_id
 
+   const res = await cloudinary.v2.uploader.destroy(imageId)
 
-})
+   if(!res) {
+       return next(new AppError('error deleting from cloudinary'))
+   }
 
-
-export const remove = asyncHandler(async (req, res, next) => {});
+    res.status(200).send('removed')
+});
