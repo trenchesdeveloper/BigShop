fbe0cdd9a99f69eab06a8d1abe5d61452b09ba3c
Damilare <opeyemisamuel222@gmail.com>
2021-02-18 17:11:02 +0000

    cloudinary configuration

--- backend/controllers/cloudinary.js
+++ backend/controllers/cloudinary.js
@@ -1,4 +1,12 @@
 import asyncHandler from "express-async-handler";
+import cloudinary from 'cloudinary'
+
+// config
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_KEY,
+  api_secret: process.env.CLOUDINARY_SECRET,
+});
 
 export const upload = asyncHandler (async (req, res, next) =>{
 
--- backend/routes/cloudinary.js
+++ backend/routes/cloudinary.js
@@ -2,10 +2,9 @@ import express from "express";
 import { remove, upload } from "../controllers/cloudinary.js";
 import { adminCheck, authCheck } from "../middlewares/auth.js";
 
-
 const router = express.Router();
 
-router.post('/uploadImages', authCheck, adminCheck, upload)
-router.post('/removeimages', authCheck, adminCheck, remove)
+router.post("/uploadImages", authCheck, adminCheck, upload);
+router.post("/removeimages", authCheck, adminCheck, remove);
 
 export default router;
