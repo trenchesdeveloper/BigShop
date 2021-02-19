63cddcec044e9db95a270a3c823c88ded0baa9d6
Damilare <opeyemisamuel222@gmail.com>
2021-02-19 17:21:36 +0000

    file resizer to resize file

--- client/src/components/forms/FileUpload.js
+++ client/src/components/forms/FileUpload.js
@@ -8,8 +8,28 @@ const FileUpload = () => {
   const { userInfo } = useSelector((state) => state.userLogin);
 
   const fileUploadAndResize = (e) => {
+    // get the files
+    let files = e.target.files;
     // resize images
-    //send back to server to upload to cloudinary
+    if (files) {
+        for (let i = 0; i < files.length; i++) {
+           Resizer.imageFileResizer(
+             files[i],
+             720,
+             720,
+             "JPEG",
+             100,
+             0,
+             (uri) => {
+               console.log(uri);
+             },
+             "base64"
+           );
+            
+        }
+    }
+
+   
     // set url to images [] in the parent component - ProductCreate
   };
 
