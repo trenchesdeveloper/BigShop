20b32dbafe7b95323b25aba74e4dbf9ee9a01060
Damilare <opeyemisamuel222@gmail.com>
2021-02-20 13:02:36 +0000

    get products endpoint

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -3,14 +3,26 @@ import Product from "../models/productModel.js";
 import AppError from "../utils/appError.js";
 
 export const create = asyncHandler(async (req, res, next) => {
-console.log(req.body.subs);
+  console.log(req.body.subs);
   const product = await Product.create(req.body);
 
   res.status(201).json(product);
 });
 
 export const getAll = asyncHandler(async (req, res, next) => {
-  const product = await Product.find({}).sort({ createdAt: -1 }).populate('category');
+  const product = await Product.find({})
+    .sort({ createdAt: -1 })
+    .populate("category");
+
+  res.status(200).json(product);
+});
+
+export const listAll = asyncHandler(async (req, res, next) => {
+  const product = await Product.find({})
+    .limit(parseInt(req.params.count))
+    .populate("subs")
+    .sort([['createdAt', 'desc']])
+    .populate("category");
 
   res.status(200).json(product);
 });
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,11 +1,12 @@
 import express from "express";
-import { create, getAll } from "../controllers/productController.js";
+import { create, getAll, listAll } from "../controllers/productController.js";
 
 import { adminCheck, authCheck } from "../middlewares/auth.js";
 
 const router = express.Router();
 
 router.route("/").get(getAll).post(authCheck, adminCheck, create);
+router.route("/:count").get(listAll); //products/100
 
 // router
 //   .route("/:slug")
