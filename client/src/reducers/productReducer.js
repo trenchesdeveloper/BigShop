1c7a84eb40feb33fe00aafbbb5f13c5def00b1e4
Damilare <opeyemisamuel222@gmail.com>
2021-02-21 08:24:09 +0000

    fetch products reducers and actions

--- client/src/reducers/productReducer.js
+++ client/src/reducers/productReducer.js
@@ -2,6 +2,9 @@ import {
   PRODUCT_CREATE_FAIL,
   PRODUCT_CREATE_REQUEST,
   PRODUCT_CREATE_SUCCESS,
+  PRODUCT_LIST_FAIL,
+  PRODUCT_LIST_REQUEST,
+  PRODUCT_LIST_SUCCESS,
 } from "../constants/productConstants";
 
 export const productCreateReducer = (state = { product: {} }, action) => {
@@ -19,3 +22,20 @@ export const productCreateReducer = (state = { product: {} }, action) => {
       return state;
   }
 };
+
+export const productListReducer = (state = { products:[] }, action) => {
+  switch (action.type) {
+    case PRODUCT_LIST_REQUEST:
+      return { loading: true, ...state };
+
+    case PRODUCT_LIST_SUCCESS:
+      return { loading: false, products: action.payload };
+
+    case PRODUCT_LIST_FAIL:
+      return { loading: false, error: action.payload };
+
+    default:
+      return state;
+  }
+};
+
