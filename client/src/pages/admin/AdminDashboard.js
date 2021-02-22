c6af892694786d33653372a5d58ed9b8460eaec1
Damilare <opeyemisamuel222@gmail.com>
2021-02-22 18:54:47 +0000

    code reorganization in admin dashboard

--- client/src/pages/admin/AdminDashboard.js
+++ client/src/pages/admin/AdminDashboard.js
@@ -1,20 +1,9 @@
 import React, { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { productListByCount } from "../../actions/productActions";
 import AdminNav from "../../components/Nav/AdminNav";
-import { LoadingOutlined } from "@ant-design/icons";
-import AdminProductCard from "../../components/cards/AdminProductCard";
+
 
 const AdminDashboard = () => {
-  const dispatch = useDispatch();
-  const { loading, error, products } = useSelector(
-    (state) => state.productListByCount
-  );
-
-  useEffect(() => {
-    dispatch(productListByCount(1));
-  }, [dispatch]);
-
+  
   return (
     <div className="container-fluid">
       <div className="row">
@@ -22,18 +11,10 @@ const AdminDashboard = () => {
           <AdminNav />
         </div>
         <div className="col">
-          {loading ? (
-            <LoadingOutlined className="h1 text-danger" />
-          ) : (
-            <h4>All Products</h4>
-          )}
+         
           <div className="col">
             <div className="row">
-              {products.map((product) => (
-                <div key={product.id} className="col-md-4">
-                  <AdminProductCard product={product} />
-                </div>
-              ))}
+             
             </div>
           </div>
         </div>
