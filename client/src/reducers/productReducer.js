f81ed0465435ea92d0cf5283f50fb22d0e1f947d
Damilare <opeyemisamuel222@gmail.com>
2021-02-23 14:37:28 +0000

    product delete reducer

--- client/src/reducers/productReducer.js
+++ client/src/reducers/productReducer.js
@@ -2,6 +2,9 @@ import {
   PRODUCT_CREATE_FAIL,
   PRODUCT_CREATE_REQUEST,
   PRODUCT_CREATE_SUCCESS,
+  PRODUCT_DELETE_FAIL,
+  PRODUCT_DELETE_REQUEST,
+  PRODUCT_DELETE_SUCCESS,
   PRODUCT_LIST_FAIL,
   PRODUCT_LIST_REQUEST,
   PRODUCT_LIST_SUCCESS,
@@ -38,4 +41,23 @@ export const productListByCountReducer = (state = { products:[] }, action) => {
       return state;
   }
 };
+
+
+
+export const productDeleteReducer = (state = {}, action) => {
+  switch (action.type) {
+    case PRODUCT_DELETE_REQUEST:
+      return { loading: true, ...state };
+
+    case PRODUCT_DELETE_SUCCESS:
+      return { loading: false, success: true};
+
+    case PRODUCT_DELETE_FAIL:
+      return { loading: false, error: action.payload };
+
+    default:
+      return state;
+  }
+};
+
 
