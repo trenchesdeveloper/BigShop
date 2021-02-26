4290213870c108aa3f6fecf6a5f3d51cd9068269
Damilare <opeyemisamuel222@gmail.com>
2021-02-26 13:56:39 +0000

    product card in home

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,18 +1,33 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { productListByCount } from "../actions/productActions";
+import ProductCard from "../components/cards/ProductCard";
 
 const Home = () => {
   const dispatch = useDispatch();
-  const { products } = useSelector((state) => state.productListByCount);
+  const { loading, error, products } = useSelector(
+    (state) => state.productListByCount
+  );
   useEffect(() => {
     dispatch(productListByCount(3));
   }, [dispatch]);
   return (
-    <div>
-      Home
-      {JSON.stringify(products)}
-    </div>
+    <>
+      <div className="jumbotron">
+        {loading ? <h4>Loading...</h4> : <h4>All Products</h4>}
+      </div>
+      <div className="container">
+        <div className="row">
+          {products.map((product) => {
+            return (
+              <div key={product._id} className="col-md-4">
+                <ProductCard product={product} />
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </>
   );
 };
 
