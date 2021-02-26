9aa813de78a0ababcc856836d79a4381288e555c
Damilare <opeyemisamuel222@gmail.com>
2021-02-26 21:06:45 +0000

    best sellers pagination

--- client/src/components/home/BestSellers.js
+++ client/src/components/home/BestSellers.js
@@ -1,4 +1,4 @@
-import { Skeleton } from "antd";
+import { Pagination } from "antd";
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { productList } from "../../actions/productActions";
@@ -6,13 +6,14 @@ import LoadingCard from "../cards/LoadingCard";
 import ProductCard from "../cards/ProductCard";
 
 const BestSellers = () => {
+  const [page, setPage] = useState(1);
   const dispatch = useDispatch();
-  const { loading, error, products } = useSelector(
+  const { loading, error, products, productsCount } = useSelector(
     (state) => state.productList
   );
   useEffect(() => {
-    dispatch(productList("sold", "desc", 3));
-  }, [dispatch]);
+    dispatch(productList("sold", "desc", page));
+  }, [dispatch, page]);
   return (
     <>
       <div className="container">
@@ -29,6 +30,16 @@ const BestSellers = () => {
             })}
           </div>
         )}
+      </div>
+
+      <div className="row">
+        <nav className="col-md-4 offset-md-4 pt-5 p-3">
+          <Pagination
+            current={page}
+            total={(productsCount / 3) * 10}
+            onChange={(value) => setPage(value)}
+          />
+        </nav>
       </div>
     </>
   );
