c12d3ca58bd929ba51941e3d1d5cfc95c84636e1
Damilare <opeyemisamuel222@gmail.com>
2021-02-27 13:08:24 +0000

    product component

--- client/src/components/cards/ProductListItems.js
+++ client/src/components/cards/ProductListItems.js
@@ -1,19 +1,89 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
+const ProductListItems = ({ product }) => {
+  const {
+    price,
+    color,
+    brand,
+    quantity,
+    category,
+    subs,
+    sold,
+    shipping,
+  } = product;
+  return (
+    <ul className="list-group">
+      <li className="list-group-item">
+        Price{" "}
+        <span className="label label-default label-pill pull-xs-right">
+          ₦ {price}
+        </span>
+      </li>
 
-const ProductListItems = ({product}) => {
-    const {price, color, brand, quantity} = product
-    return (
-      <ul className="list-group">
+      {category && (
         <li className="list-group-item">
-          Price{" "}
-          <span className="label label-default label-pill pull-xs-right">
-            ₦ {price}
-          </span>
+          Category{" "}
+          <Link
+            to={`/category/${category.slug}`}
+            className="label label-default label-pill pull-xs-right"
+          >
+            {category.name}
+          </Link>
         </li>
-      </ul>
-    );
-}
+      )}
 
-export default ProductListItems
+      {subs && (
+        <li className="list-group-item">
+          Sub Categories
+          {subs.map((s) => (
+            <Link
+              key={s._id}
+              to={`/subs/${s.slug}`}
+              className="label label-default label-pill pull-xs-right"
+            >
+              {s.name}
+            </Link>
+          ))}
+        </li>
+      )}
+
+      <li className="list-group-item">
+        Shipping{" "}
+        <span className="label label-default label-pill pull-xs-right">
+          {shipping}
+        </span>
+      </li>
+
+      <li className="list-group-item">
+        Color{" "}
+        <span className="label label-default label-pill pull-xs-right">
+         {color}
+        </span>
+      </li>
+
+      <li className="list-group-item">
+        Brand{" "}
+        <span className="label label-default label-pill pull-xs-right">
+          {brand}
+        </span>
+      </li>
+
+      <li className="list-group-item">
+        Available {" "}
+        <span className="label label-default label-pill pull-xs-right">
+          {quantity}
+        </span>
+      </li>
+
+      <li className="list-group-item">
+        Sold{" "}
+        <span className="label label-default label-pill pull-xs-right">
+         {sold}
+        </span>
+      </li>
+    </ul>
+  );
+};
+
+export default ProductListItems;
