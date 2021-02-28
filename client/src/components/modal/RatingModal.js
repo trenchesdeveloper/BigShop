07347b05fcc05c3bce179b8044812c81e734dcbf
Damilare <opeyemisamuel222@gmail.com>
2021-02-28 20:22:00 +0000

    login to leave rating

--- client/src/components/modal/RatingModal.js
+++ client/src/components/modal/RatingModal.js
@@ -3,15 +3,26 @@ import { Modal, Button } from "antd";
 import { useDispatch, useSelector } from "react-redux";
 import { toast } from "react-toastify";
 import { StarOutlined } from "@ant-design/icons";
+import { useHistory } from "react-router-dom";
 
 const RatingModal = ({ children }) => {
   const [modalVisible, setModalVisible] = useState(false);
 
+  const history = useHistory();
+
   const { userInfo } = useSelector((state) => state.userLogin);
+
+  const handleModal = () => {
+    if (userInfo && userInfo.token) {
+      setModalVisible(true);
+    } else {
+      history.push("/login");
+    }
+  };
 
   return (
     <>
-      <div onClick={() => setModalVisible(true)}>
+      <div onClick={handleModal}>
         <StarOutlined className="text-danger" /> <br />
         {userInfo ? "Leave Rating" : "Login to Leave Rating"}
       </div>
@@ -22,9 +33,9 @@ const RatingModal = ({ children }) => {
         visible={modalVisible}
         onOk={() => {
           setModalVisible(false);
-        toast.success('Thanks for your review')}}
-
-        onCancel={()=> setModalVisible(false)}
+          toast.success("Thanks for your review");
+        }}
+        onCancel={() => setModalVisible(false)}
       >
         {children}
       </Modal>
